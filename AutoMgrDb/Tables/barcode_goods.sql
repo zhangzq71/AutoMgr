24daff74de6a1c61c09cdd95b99fdbfb85571cfb
zhangzq71 <zhangzq71@hotmail.com>
2014-02-21 14:03:11 +0800

    add barcode_goods table

---
+++ AutoMgrDb/Tables/barcode_goods.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[barcode_goods]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [barcode] VARCHAR(50) NOT NULL, 
+    [goods_id] INT NOT NULL, 
+    CONSTRAINT [FK_barcode_goods_goods] FOREIGN KEY ([goods_id]) REFERENCES [goods]([id]),
+)
