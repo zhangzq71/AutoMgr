f624584555f0c0c138160bd843d2eb79e778bb9f
zhangzq71 <zhangzq71@hotmail.com>
2014-03-24 17:11:29 +0800

    正在实现修车界面

---
+++ AutoMgrW8/Converter/DepartmentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using System.Data.Services.Client;
+
+namespace AutoMgrW8.Converter
+{
+    public class DepartmentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return "请点击这里安排班组";
+            else
+                return ((AutoMgrSvc.department)value).name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AutoMgrW8/ViewModel/VMRepairFty.cs
+++ AutoMgrW8/ViewModel/VMRepairFty.cs
@@ -16,6 +16,7 @@ namespace AutoMgrW8.ViewModel
 {
     public class VMRepairFty : ViewModelBase
     {
+        private AutoMgrSvc.repair_item_detail _currentRepairItemDetail;
         private readonly INavigationService _navigationService;
         private readonly AutoMgrSvc.AutoMgrDbEntities _context = new AutoMgrSvc.AutoMgrDbEntities(new Uri("http://192.168.0.101:23796/Service/AutoMgrDbSvc.svc/"));
 
@@ -34,6 +35,17 @@ namespace AutoMgrW8.ViewModel
             }
         }
 
+        public bool IsPopupOpen
+        {
+            get { return _isPopupOpen; }
+            private set
+            {
+                _isPopupOpen = value;
+                RaisePropertyChanged();
+            }
+        }
+        private bool _isPopupOpen;
+
         public bool Isediting
         {
             get { return _isEditing; }
@@ -44,6 +56,28 @@ namespace AutoMgrW8.ViewModel
             }
         }
         private bool _isEditing;
+
+        public CollectionViewSource Departments
+        {
+            get { return _departments; }
+            private set
+            {
+                _departments = value;
+                RaisePropertyChanged();
+            }
+        }
+        private CollectionViewSource _departments;
+
+        public CollectionViewSource Repairs
+        {
+            get { return _repairs; }
+            private set
+            {
+                _repairs = value;
+                RaisePropertyChanged();
+            }
+        }
+        private CollectionViewSource _repairs;
 
         public CollectionViewSource RepairItems
         {
@@ -137,21 +171,24 @@ namespace AutoMgrW8.ViewModel
         /// <summary>
         /// 搜索命令
         /// </summary>
-        public RelayCommand CommandFilter
+        public RelayCommand<AutoMgrSvc.repair_item_detail> CommandSelectDepartment
         {
             get
             {
-                if (_commandFilter == null)
+                if (_commandSelectDepartment == null)
                 {
-                    _commandFilter = new RelayCommand(() =>
+                    _commandSelectDepartment = new RelayCommand<AutoMgrSvc.repair_item_detail>((val) =>
                     {
+                        IsPopupOpen = !IsPopupOpen;
+
+                        _currentRepairItemDetail = val;
                     });
                 }
 
-                return _commandFilter;
+                return _commandSelectDepartment;
             }
         }
-        private RelayCommand _commandFilter;
+        private RelayCommand<AutoMgrSvc.repair_item_detail> _commandSelectDepartment;
 
         public RelayCommand CommandRefresh
         {
@@ -183,12 +220,29 @@ namespace AutoMgrW8.ViewModel
                         });
                         var qry1 = from item in _context.repair_item select item;
                         repairItems.LoadAsync(qry1);
+
+                        var departments = new DataServiceCollection<AutoMgrSvc.department>();
+                        departments.LoadCompleted += new EventHandler<LoadCompletedEventArgs>((sender, e) =>
+                        {
+                            if (e != null)
+                            {
+                                Departments = new CollectionViewSource() { Source = departments };
+                                Departments.View.CurrentChanged += new EventHandler<object>((s1, e1) =>
+                                {
+                                    _currentRepairItemDetail.department = (AutoMgrSvc.department)Departments.View.CurrentItem;
+                                    _currentRepairItemDetail.department_id = _currentRepairItemDetail.department.id;
+                                });
+                            }
+                        });
+                        var qry2 = from department in _context.department select department;
+                        departments.LoadAsync(qry2);
                     });
                 }
 
                 return _commandRefresh;
             }
         }
+
         private RelayCommand _commandRefresh;
 
         public RelayCommand<string> CommandButton
@@ -212,26 +266,78 @@ namespace AutoMgrW8.ViewModel
                                     parts_price = 0,
                                     discount = 0,
                                     real_price = 0,
-                                    vehicle = new AutoMgrSvc.vehicle() 
-                                    { 
+                                    vehicle = new AutoMgrSvc.vehicle()
+                                    {
                                         car_num = "12345",
                                         engine_num = "54321",
                                         frame_num = "9876543",
-                                        brand = "东风日产",
-                                        model = "A100",
-                                        customer = new AutoMgrSvc.customer() 
+                                        brand = "东风日产2",
+                                        model = "A200",
+                                        customer = new AutoMgrSvc.customer()
                                         {
-                                            company = "第一运输公司",
+                                            company = "第3运输公司",
                                             address = "广州市",
                                             phone = "13316161002",
-                                        }, 
+                                        },
                                     },
                                 };
+
                                 Isediting = true;
+
+                                //_context.AddTocustomer(RepairDetail.vehicle.customer);
+                                //_context.AddTovehicle(RepairDetail.vehicle);
+                                //_context.SetLink(RepairDetail.vehicle, "customer", RepairDetail.vehicle.customer);
+
+                                //_context.AddTorepair(RepairDetail);
+                                //var repair_item = new AutoMgrSvc.repair_item_detail() { department_id = 6, item_id = 1 };
+                                //var repair_part = new AutoMgrSvc.repair_parts() { goods_id = 1, price = 100.00m, quote_only = false, };
+                                //var repair_part1 = new AutoMgrSvc.repair_parts() { goods_id = 2, price = 120.00m, quote_only = false, };
+
+                                //RepairDetail.repair_item_detail.Add(repair_item);
+                                //RepairDetail.repair_parts.Add(repair_part);
+                                //RepairDetail.repair_parts.Add(repair_part1);
+
+                                //_context.AddTorepair_parts(repair_part);
+                                //_context.AddTorepair_parts(repair_part1);
+                                //_context.AddTorepair_item_detail(repair_item);
+
+                                //_context.SetLink(RepairDetail, "vehicle", RepairDetail.vehicle);
+                                //_context.SetLink(repair_item, "repair", RepairDetail);
+                                //_context.SetLink(repair_part, "repair", RepairDetail);
+                                //_context.SetLink(repair_part1, "repair", RepairDetail);
+                                //_context.AddLink(RepairDetail, "repair_item_detail", repair_item);
+                                //_context.AddLink(RepairDetail, "repair_parts", repair_part);
+                                //_context.AddLink(RepairDetail, "repair_parts", repair_part1);
+                                //_context.BeginSaveChanges(result => { _context.EndSaveChanges(result); }, null);
                                 break;
 
                             case "取消接车":
                                 Isediting = false;
+                                break;
+
+                            case "添加项目":
+                                RepairDetail.repair_item_detail.Add(new AutoMgrSvc.repair_item_detail() { item_id = ((AutoMgrSvc.repair_item)RepairItems.View.CurrentItem).id, repair_item = (AutoMgrSvc.repair_item)RepairItems.View.CurrentItem, });
+                                break;
+
+                            case "添加类型":
+                                break;
+
+                            case "测试":
+                                IsPopupOpen = !IsPopupOpen;
+                                break;
+
+                            case "搜索":
+                                var repairs = new DataServiceCollection<AutoMgrSvc.repair>();
+                                repairs.LoadCompleted += new EventHandler<LoadCompletedEventArgs>((sender, e) => 
+                                {
+                                    if (e.Error == null)
+                                    {
+                                        Repairs = new CollectionViewSource() { Source = repairs };
+                                    }
+                                });
+                                //var query = from repair in _context.repair.Expand("vehicle/customer").Expand("staff").Expand("repair_item_detail/repair_item").Expand("repair_item_detail/department").Expand("repair_parts/goods") select repair;
+                                var query = from repair in _context.repair.Expand("vehicle/customer").Expand("staff").Expand("repair_item_detail/repair_item").Expand("repair_item_detail/department").Expand("repair_parts/goods") select repair;
+                                repairs.LoadAsync(query);
                                 break;
                         }
                     });
